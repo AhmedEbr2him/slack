ece199336fe60e819051a7efd5a1ae99acd988af
AhmedEbrahim <89828478+AhmedEbr2him@users.noreply.github.com>
2024-11-30 21:16:45 +0200

    add message toolbar

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -9,6 +9,7 @@ import {
   AvatarFallback
 } from './ui/avatar';
 import { Thumbnail } from './thumbnail';
+import { Toolbar } from './toolbar';
 
 const Renderer = dynamic(() => import("@/components/renderer"), { ssr: false });
 
@@ -133,6 +134,18 @@ export const Message = ({
           ) : null}
         </div>
       </div>
+
+      {!isEditing && (
+        <Toolbar
+          isAuthor={isAuthor}
+          isPending={false}
+          handleEdit={() => setEditingId(id)}
+          handleThread={() => { }}
+          handleDelete={() => { }}
+          handleReaction={() => { }}
+          hideThreadButton={hideThreadButton}
+        />
+      )}
     </div>
   )
 }
